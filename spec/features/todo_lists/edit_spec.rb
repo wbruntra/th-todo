0d0351ca32386f4737a7ba156ee0622da92ef51f
William Bruntrager <w.bruntrager@gmail.com>
2015-12-10 16:41:50 -0500

    new test integrated

---
+++ spec/features/todo_lists/edit_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Editing todo lists" do
+  it "updates a todo list successfully with correct information" do
+    todo_list = TodoList.create(title: "Groceries", description: "Grocery list.")
+    visit "/todo_lists"
+    within "#todo_lists_#{todo_list.id}" do
+      click_link "Edit"
+    end
+    
+    fill_in "Title", with: "New title"
+    fill_in "Description", with: "New description"
+    click_button "Update Todo list"
+    
+    expect(page).to have_content("Todo list was successfully updated")
+    expect(todo_list.title).to eq("New title")
+    expect(todo_list.description).to eq("New description")
+  end
+end
+
+    
